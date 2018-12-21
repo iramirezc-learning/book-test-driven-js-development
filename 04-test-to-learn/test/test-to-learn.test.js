23994e3c5d9755d7aa89d7f4f3ffbd8a3a11b908
Isaac Ramirez <iramirezc@live.com.mx>
2018-12-21 03:01:08 -0600

    rename tests for chap 4

---
+++ 04-test-to-learn/test/test-to-learn.test.js
@@ -0,0 +1,21 @@
+/* globals describe it */
+
+let assert = require('assert')
+
+describe('Chapter 04', () => {
+  describe('Array', function () {
+    describe('#splice()', function () {
+      it('should modify array', function () {
+        let arr = [1, 2, 3, 4, 5]
+        arr.splice(2, 3)
+        assert.deepStrictEqual(arr, [1, 2])
+      })
+
+      it('should return removed items from array', function () {
+        let arr = [1, 2, 3, 4, 5]
+        let result = arr.splice(2, 3)
+        assert.deepStrictEqual(result, [3, 4, 5])
+      })
+    })
+  })
+})
