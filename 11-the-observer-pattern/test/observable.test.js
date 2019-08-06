6caddd288178d977595c6a818c1bc086ac787b96
Isaac Ramirez <iramirezc@live.com.mx>
2019-08-05 22:20:50 -0500

    refactor observable tests

--- 11-the-observer-pattern/test/observable.test.js
+++ 11-the-observer-pattern/test/observable.test.js
@@ -2,58 +2,21 @@
 
 const assert = require('assert')
 
-const Observable = require('../code/observable')
+const Observable = require('../src/observable')
 
 describe('Chapter 11', () => {
-  let observable
+  describe('Observable', () => {
+    let observable
 
-  beforeEach(() => {
-    observable = Object.create(Observable)
-  })
-
-  describe('11.1 The Observer in JavaScript', () => {
-    it('should be nothing to test', () => {
-      assert.strictEqual(true, true)
+    beforeEach(() => {
+      observable = Object.create(Observable)
     })
-  })
 
-  describe('11.2 Adding Observers', () => {
-    describe('Observable - observe', () => {
-      it('should store a function', () => {
-        const observers = [
-          function () { /* noop */ },
-          function () { /* noop */ }
-        ]
-
-        observable.observe('event', observers[0])
-        observable.observe('event', observers[1])
-
-        // NOTE: observers is not longer public
-        // assert.deepStrictEqual(observers, observable.observers)
-        assert.strictEqual(observable.hasObserver('event', observers[0]), true)
-        assert.strictEqual(observable.hasObserver('event', observers[1]), true)
-      })
-    })
-  })
-
-  describe('11.3 Checking for Observers', () => {
-    describe('Observable - hasObserver', () => {
-      it('should return true when has observer', () => {
-        const observer = function () { /* noop */ }
-
-        observable.observe('event', observer)
-
-        assert.deepStrictEqual(observable.hasObserver('event', observer), true)
-      })
-
+    describe('observe & hasObserver - methods', () => {
       it('should return false when has no observers', () => {
         assert.deepStrictEqual(observable.hasObserver('event', function () { }), false)
       })
-    })
 
-    describe('Observable - add & has observer', () => {
-      // this is a combination of duplicated tests
-      // observe and hasObserver
       it('should store functions', () => {
         const observers = [
           function () { /* noop */ },
@@ -66,11 +29,15 @@ describe('Chapter 11', () => {
         assert.deepStrictEqual(observable.hasObserver('event', observers[0]), true)
         assert.deepStrictEqual(observable.hasObserver('event', observers[1]), true)
       })
-    })
-  })
 
-  describe('11.4 Notifying Observers', () => {
-    describe('Observable - notify', () => {
+      it('should throw for an un-callable observer', () => {
+        assert.throws(() => {
+          observable.observe('event', {})
+        }, /TypeError/)
+      })
+    })
+
+    describe('notify - method', () => {
       it('should call all observers', () => {
         const observer1 = function () { observer1.called = true }
         const observer2 = function () { observer2.called = true }
@@ -94,19 +61,7 @@ describe('Chapter 11', () => {
 
         assert.deepStrictEqual(actual, ['String', 1, true])
       })
-    })
-  })
 
-  describe('11.5 Error Handling', () => {
-    describe('Observable - observe', () => {
-      it('should throw for an un-callable observer', () => {
-        assert.throws(() => {
-          observable.observe('event', {})
-        }, /TypeError/)
-      })
-    })
-
-    describe('Observable - notify', () => {
       it('should notify all even when some fail', () => {
         const observer1 = function () { throw new Error('Oops!') }
         const observer2 = function () { observer2.called = true }
@@ -129,21 +84,13 @@ describe('Chapter 11', () => {
 
         assert.deepStrictEqual(calls, [observer1, observer2])
       })
-    })
-  })
 
-  describe('11.6 Observing Arbitrary Objects', () => {
-    describe('Observable - notify', () => {
       it('should not fail if no observers', () => {
         assert.doesNotThrow(() => {
           observable.notify('event')
         })
       })
-    })
-  })
 
-  describe('11.7 Observing Arbitrary Events', () => {
-    describe('Observable - observe', () => {
       it('should notify relevant observers only', () => {
         const calls = []
 
